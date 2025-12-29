8df85a95b8a180707b3c4289365eaf57a17fa113
cey0 <cey0@proton.me>
2025-12-29 17:11:35 +0700

    fix the text thingy

--- src/app/components/section/ProjectSection.tsx
+++ src/app/components/section/ProjectSection.tsx
@@ -4,8 +4,8 @@ export default function ProjectSection() {
   return (
     <section id="Projects" className="mt-12 md:mt-16 lg:mt-20 relative z-10">
       <div className="flex flex-col gap-8 md:gap-12">
-        <h2 className="text-2xl md:text-3xl lg:text-h3 font-semibold leading-tight max-w-4xl">
-          Join our team and shape the future with innovative solutions.
+        <h2 className="text-h3 font-semibold leading-tight max-w-4xl">
+         A showcase of the projects we proudly build.
         </h2>
         <StickyScroll />
       </div>
